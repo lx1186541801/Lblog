9635e2449677f417fbe77a0afe565952c959c03e
Robert.LJ <1186541801@qq.com>
2021-04-28 07:44:14 +0000

    Initial Style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,8 +4,23 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>@yield('title', 'LBLOG App') - LBLOG App</title>
+	<link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-	@yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">LBLOG App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+      </div>
+    </nav>
+
+
+	<div class="container">
+		@yield('content')
+	</div>
 </body>
 </html>
--- resources/views/static_pages/index.blade.php
+++ resources/views/static_pages/index.blade.php
@@ -3,5 +3,19 @@
 
 @section('title', '首页')
 @section('content')
-<h1>index</h1>
+
+<div class="jumbotron">
+	<h1>Hello LBLOG</h1>
+	<p class="lead">
+		你看的页面是<a href="/">测试</a>页面
+	</p>
+	<p>
+		开始，从这里！
+	</p>
+	<p>
+		<a class="btn btn-lg btn-success" href="#" role="button">SIGN UP</a>	
+	</p>
+</div>
+
+
 @stop
