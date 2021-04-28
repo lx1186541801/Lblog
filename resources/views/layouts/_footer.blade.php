58487f8bfc016c2b8b922d9b542c5016f2cad6a2
Robert.LJ <1186541801@qq.com>
2021-04-28 08:00:08 +0000

    create header and footer blade

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+	<img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="" >
+	<a href="/" target=_blank>
+		EveryDay is New Day
+	</a>
+
+	<div class="float-right">
+		<a href="/about">About</a>
+	</div>
+</footer>
