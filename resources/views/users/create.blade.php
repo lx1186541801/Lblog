1c597bf922d663435de60f217d774900f714feb5
Robert.LJ <1186541801@qq.com>
2021-04-30 06:21:35 +0000

    create user form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,5 +3,40 @@
 @section('title', 'SignUp')
 
 @section('content')
-<h1>signup</h1>
+
+<div class="offset-md-2 col-md-8">
+	<div class="card">
+		<div class="card-header">
+			<h5>SIGN UP</h5>
+		</div>
+		<div class="card-body">
+			<form action="{{ route('users.store') }}" method="POST">
+				<div class="form-group">
+					<label for="name">名称</label>
+					<input type="text" name="name" class="form-control" value="{{ old('name') }}">
+				</div>
+
+				<div class="form-group">
+					<label for="name">邮箱</label>
+					<input type="text" name="email" class="form-control" value="{{ old('email') }}">
+				</div>
+
+				<div class="form-group">
+					<label for="name">密码</label>
+					<input type="text" name="password" class="form-control" value="{{ old('password') }}">
+				</div>
+
+				<div class="form-group">
+					<label for="name">确认密码</label>
+					<input type="text" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+				</div>
+
+
+				<button class="btn btn-primary" type="submit">注册</button>
+			</form>
+		</div>
+	</div>
+</div>
+
+
 @stop
