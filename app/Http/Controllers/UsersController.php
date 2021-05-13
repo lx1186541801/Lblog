ea40c643c16dd743ea1879292ba7b64ad0796f2f
Robert.LJ <1186541801@qq.com>
2021-05-13 06:44:41 +0000

    change user info

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -40,4 +40,31 @@ class UsersController extends Controller
     	session()->flash('success', '欢迎！你将在这里开始一段新的旅程！');
     	return redirect()->route('users.show', [$user]);
     }
+
+    public function edit(User $user)
+    {
+    	return view('users.edit', compact('user'));
+    }
+
+
+    public function update(User $user, Request $request)
+    {
+    	$this->validate($request, [
+    		'name' => 'required|unique:users|max:50',
+    		'password' => 'nullable|confirmed|min:6'
+    	]);
+
+    	$data = [];
+
+    	$data['name'] = $request->name;
+    	if ($request->password) {
+    		$data['password'] = $request->password;
+    	}
+
+    	$user->update($data);
+
+    	session()->flash('success', '个人资料编辑成功！');
+
+    	return redirect()->route('users.show', $user);
+    }
 }
