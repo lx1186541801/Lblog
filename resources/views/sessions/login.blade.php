13e410af1ad1f63d709d2d2b71a894a8574c8d37
Robert.LJ <1186541801@qq.com>
2021-05-13 02:00:54 +0000

    login and  remember me

--- resources/views/sessions/login.blade.php
+++ resources/views/sessions/login.blade.php
@@ -23,6 +23,13 @@
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
           </div>
 
+          <div class="form-group">
+          	<div class="form-check">
+          		<input type="checkbox" class="form-check-input" name="remember" id="check01">
+          		<label for="check01" class="form-check-label">记住我</label> 
+          	</div>
+          </div>
+
           <button type="submit" class="btn btn-primary">登录</button>
       </form>
 
