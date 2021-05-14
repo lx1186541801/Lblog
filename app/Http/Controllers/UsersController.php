0cd6d3285bab8b255d4f1b7c4d8d3c15de11a6cd
Robert.LJ <1186541801@qq.com>
2021-05-14 06:53:29 +0000

    use policy

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,22 @@ use Auth;
 
 class UsersController extends Controller
 {
+
+
+	public function __construct()
+	{
+		$this->middleware('auth', [
+			'except' => ['show', 'create', 'store']
+		]);
+
+		$this->middleware('guest', [
+			'only' => ['create']
+		]);
+
+	}
+
+
+
     public function create()
     {
         return view('users.create');
@@ -43,12 +59,16 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+    	$this->authorize('update', $user);
+
     	return view('users.edit', compact('user'));
     }
 
 
     public function update(User $user, Request $request)
     {
+    	$this->authorize('update', $user);
+
     	$this->validate($request, [
     		'name' => 'required|unique:users|max:50',
     		'password' => 'nullable|confirmed|min:6'
