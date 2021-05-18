0c165cc59ca6cb6a7b7d341c708f5cb43254a217
Robert.LJ <1186541801@qq.com>
2021-05-18 08:59:42 +0000

    reset password

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -57,9 +57,58 @@ class PasswordController extends Controller
     public function showResetForm($token)
     {
     	
-    	return view('auth.password.reset', $token);
+    	return view('auth.passwords.reset', compact('token'));
     }
 
+
+
+    public function reset(Request $request)
+    {
+    	$request->validate([
+    		'token' => 'required',
+    		'email' => 'required|email',
+    		'password' => 'required|confirmed|min:6'
+    	]);
+
+    	$email = $request->email;
+    	$token = $request->token;
+
+
+    	$expired = 60 * 10;
+
+    	$user = User::where('email', $email)->first();
+
+
+    	if (is_null($user)) {
+    		session()->flash('danger', '邮箱未注册');
+    		return redirect()->back()->withInput();
+    	}
+
+    	$record = DB::table('password_resets')->where('email', $email)->first();
+
+    	if ($record) {
+    		
+    		if (Carbon::parse($record->created_at)->addSeconds($expired)->isPast()) {
+    			session()->flash('danger', '链接已过期，请重新尝试');
+    			return redirect()->route('password.request');
+    		}
+
+    		if ( ! Hash::check($token, $record->token)) {
+    			session()->flash('danger', '令牌错误，请重新尝试');
+    			return redirect()->back();
+    		}
+
+    		$user->update(['password' => bcrypt($request->password)]);
+
+    		session()->flash('success', '密码重置成功，请使用新密码登陆');
+
+    		return redirect()->route('login');
+    	}
+
+
+    	session()->flash('danger', '未找到重置记录');
+    	return redirect()->back();
+    }
 
 
 }
